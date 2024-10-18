60bdac3291558a4e5d11f51791687fd9113c8538
cdrani <charlesdrani@gmail.com>
2024-10-17 23:15:12 -0600

    chore: prettify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import { crx } from '@crxjs/vite-plugin';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import type { ManifestV3Export } from '@crxjs/vite-plugin';
+import path from 'path'
+import { defineConfig } from 'vite'
+import { crx } from '@crxjs/vite-plugin'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import type { ManifestV3Export } from '@crxjs/vite-plugin'
 
-import manifest from './manifest.json';
+import manifest from './manifest.json'
 
 export default defineConfig({
-	resolve: {
-		alias: {
-			$lib: path.resolve(__dirname, '/src/lib')
-		}
-	},
-	plugins: [svelte(), crx({ manifest: manifest as unknown as ManifestV3Export })],
-	server: {
-		port: 5173,
-		strictPort: true,
-		hmr: {
-			port: 5173
-		}
-	}
-});
+    resolve: {
+        alias: {
+            $lib: path.resolve(__dirname, '/src/lib')
+        }
+    },
+    plugins: [svelte(), crx({ manifest: manifest as unknown as ManifestV3Export })],
+    server: {
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            port: 5173
+        }
+    }
+})
