315fc3d77d215e30798a61ad3080d07d266db5ec
cdrani <charlesdrani@gmail.com>
2024-10-17 23:17:35 -0600

    docs: add RecorderState interface

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,20 @@
 export interface Settings {
-	muted: boolean;
-	format: 'webm' | 'mp3' | 'wav';
-	quality: number;
-	microphoneId: string | null;
+    muted: boolean
+    format: 'webm' | 'mp3' | 'wav'
+    quality: number
+    microphoneId: string | null
 }
 
 export interface Tab {
-	id?: number;
-	url?: string;
-	title?: string;
+    id?: number
+    url?: string
+    title?: string
+}
+
+export interface RecorderState {
+    active: boolean
+    isRecording: boolean
+    isPaused: boolean
+    isMuted: boolean
+    audioUrl: string | null
 }
