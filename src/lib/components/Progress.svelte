ca5f8e9679b3fb1113b162143e9c89fca230ac4c
cdrani <charlesdrani@gmail.com>
2024-10-21 07:27:48 -0600

    style: update Progress ui + spacing

--- src/lib/components/Progress.svelte
+++ src/lib/components/Progress.svelte
@@ -25,14 +25,18 @@
             waveColor: 'rebeccapurple',
             progressColor: 'rebeccapurple',
             cursorColor: 'rebeccapurple',
-            height: 50
+            cursorWidth: 2,
+            height: 56,
+            barWidth: 2,
+            normalize: true,
+            barHeight: 1.5,
+            minPxPerSec: 10
         })
 
         wavesurfer.on('play', () => (isPlaying = true))
         wavesurfer.on('pause', () => (isPlaying = false))
         wavesurfer.on('audioprocess', (time) => (currentTime = time))
         wavesurfer.on('ready', () => (duration = wavesurfer.getDuration()))
-        wavesurfer.on('destroy', () => console.log('WaveSurfer destroyed'))
     }
 
     onMount(() => {
@@ -82,29 +86,35 @@
         wavesurfer.skip(-5)
     }
 
-    function formatTime(time: number, includeHours: boolean = false): string {
+    function formatTime(time: number): string {
         const hours = Math.floor(time / 3600)
         const minutes = Math.floor((time % 3600) / 60)
         const seconds = Math.floor(time % 60)
-        if (includeHours) {
-            return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+
+        const shortTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+        if (hours > 0) {
+            return `${hours < 9 ? hours : hours.toString().padStart(2, '0')}:${shortTime}`
         }
-        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+        return shortTime
     }
 </script>
 
-<div class="mb-4 w-full rounded-md bg-secondary p-4">
-    <div bind:this={waveformContainer} class="h-16 w-full rounded-md" />
+<div class="w-full space-y-2 rounded-md bg-secondary p-4">
+    <div bind:this={waveformContainer} class="h-14 w-full rounded-md" />
 
     {#if isRecording}
-        <p class="text-xl font-bold text-muted-foreground">{formatTime(duration, true)}</p>
+        <p class="text-lg font-bold text-muted-foreground">{formatTime(duration)}</p>
     {/if}
 
     {#if isActive && audioUrl && !isRecording}
         <div class="flex items-center justify-between">
-            <span>{formatTime(currentTime)} / {formatTime(duration)}</span>
+            <div class="flex-col">
+                <p class="text-sm text-muted-foreground">{formatTime(currentTime)}</p>
+                <hr class="my-0.5 border-muted-foreground" />
+                <p class="text-sm text-muted-foreground">{formatTime(duration)}</p>
+            </div>
             <div class="space-x-2">
-                <Button on:click={skipBackward} variant="outline" size="icon">
+                <Button on:click={skipBackward} class="h-8 w-8" variant="outline" size="icon">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -123,7 +133,7 @@
                         ></line></svg
                     >
                 </Button>
-                <Button on:click={togglePlayPause} variant="outline" size="icon">
+                <Button on:click={togglePlayPause} class="h-8 w-8" variant="outline" size="icon">
                     {#if isPlaying}
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -157,7 +167,7 @@
                         >
                     {/if}
                 </Button>
-                <Button on:click={skipForward} variant="outline" size="icon">
+                <Button on:click={skipForward} class="h-8 w-8" variant="outline" size="icon">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -176,7 +186,7 @@
                         ></line></svg
                     >
                 </Button>
-                <Button on:click={restart} variant="outline" size="icon">
+                <Button on:click={restart} class="h-8 w-8" variant="outline" size="icon">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
